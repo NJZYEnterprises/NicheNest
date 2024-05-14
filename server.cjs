7165f7e3884a824682ce29d1ba1fd6c58faa87fe
Jreinert0414 <jim.reinert.ii@gmail.com>
2024-05-14 13:26:57 -0500

    addded morgan and cors middleware

--- server.cjs
+++ server.cjs
@@ -1,12 +1,26 @@
+const cors = require("cors")
+const morgan = require("morgan")
 const express = require("express")
 require('dotenv').config();
 const server = express();
 const path = require("path");
 
+
+server.use(morgan("dev")) // Morgan for logging HTTP requests
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+server.use(cors())
+
 const apiRouter = require("./backend/api/index.cjs")
 const authRouter = require("./backend/auth/index.cjs")
 
 
+server.use((req, res, next) => {
+  console.log("<---- Body Logger START ---->")
+  console.log(req.body)
+  console.log("<----- Body Logger END ----->")
+  next()
+}) 
 
 server.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/dist/index.html')
