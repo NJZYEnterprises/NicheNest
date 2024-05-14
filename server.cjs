342f6a206482e4858b8e79f92286377dc578043f
Jreinert0414 <jim.reinert.ii@gmail.com>
2024-05-14 12:15:48 -0500

    server up dist folder

--- server.cjs
+++ server.cjs
@@ -1,9 +1,19 @@
 const express = require("express")
 require('dotenv').config();
 const server = express();
+const path = require("path");
 
 const apiRouter = require("./backend/api/index.cjs")
 const authRouter = require("./backend/auth/index.cjs")
+
+
+
+server.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
+server.use(express.static(path.join(__dirname, './dist')))
+
 
 server.use("/api", apiRouter)
 server.use("/auth", authRouter)
