fe29b223870e6b180aaec31c9098c91160b84b94
Jreinert0414 <jim.reinert.ii@gmail.com>
2024-05-14 15:44:08 -0500

    woring on freelancer route

--- backend/api/users.cjs
+++ backend/api/users.cjs
@@ -1,10 +1,30 @@
+const { PrismaClient} = require('@prisma/client');
 const express = require('express')
 const userRouter = express.Router();
 
-userRouter.use("/users", userRouter)
+const prisma = new PrismaClient();
+
+userRouter.use("/users/", userRouter)
 
 userRouter.get("/", (req, res, next)=>{
   res.send(`this is the users route`)
 })
+
+userRouter.get('/freelancers/', async(req, res, next)=>{
+  try{
+    const freelancers = await prisma.user.findMany ({
+      where:{
+        services: !null
+      }
+    })
+    res.send(freelancers)
+  }catch(error){
+    console.log(error);
+    res.sendStatus(501)
+  }
+})
+
+
+
 
 module.exports = userRouter
