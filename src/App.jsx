24f2153f126d41f97576665f41ac024842ee7076
ypatel0101 <160627473+ypatel0101@users.noreply.github.com>
2024-05-14 14:06:07 -0500

    Revert "added login, imported them in App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
 import "./App.css"
 
 function App() {
   return (
     <>
       <h1 className="text-red-400">Welcome to Niche Nest!</h1>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
     </>
   )
 }
