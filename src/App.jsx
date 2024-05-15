a81e213bd57f92c94cab28c5ab28ba381b0c0ac3
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-05-15 09:22:17 -0500

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./auth/authContext"
 import Login from "./components/Login"
 import Register from "./components/Register"
 import Home from "./pages/Home"
@@ -8,11 +9,13 @@ import "./App.css"
 function App() {
   return (
     <>
+    <AuthProvider>
       <Routes>
         <Route index="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      </AuthProvider>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App.jsx"
 import "./index.css"
-import { AuthProvider } from "./auth/authContext"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </BrowserRouter>
 )
