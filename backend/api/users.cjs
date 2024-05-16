09ce79b7f25ba4f4593ffe0f70a9d40f4530559d
Jreinert0414 <jim.reinert.ii@gmail.com>
2024-05-16 11:03:51 -0500

    working user routes

--- backend/api/users.cjs
+++ backend/api/users.cjs
@@ -6,38 +6,86 @@ const prisma = require('../db/connection.cjs')
 
 userRouter.use("/users/", userRouter)
 
-userRouter.get("/", (req, res, next)=>{
-  res.send(`this is the users route`)
-})
+const myPrisma = require('../db/myPrisma.cjs');
 
-userRouter.get('/freelancers/', async(req, res, next)=>{
-  try{
-    const freelancers = await prisma.user.findMany ({
-      where:{
-        services: {some:{}}
+
+userRouter.get('/freelancers/', async (req, res, next) => {
+  try {
+    const freelancers = await prisma.user.findMany({
+      where: {
+        services: { some: {} }
       }
     })
     res.send(freelancers)
-  }catch(error){
+  } catch (error) {
     next(error)
   }
 })
 
-userRouter.get('/freelancers/:id', async(req, res, next)=>{
-  try{
-    const freelancers = await prisma.user.findUnique ({
-      where:{
-        services: {some:{}},
-        id: parseInt(req.params.id) 
+userRouter.get('/freelancers/:id', async (req, res, next) => {
+  try {
+    const freelancers = await prisma.user.findUnique({
+      where: {
+        services: { some: {} },
+        id: parseInt(req.params.id)
       }
     })
     res.send(freelancers)
-  }catch(error){
+  } catch (error) {
     next(error)
   }
 })
 
 
 
+userRouter.get('/', async (req, res, next)=> {
+  try {
+    const allUsers = await prisma.user.findMany();
+    res.send(allUsers);
+   } catch (error) {
+    next(error)
+  }
+}) 
 
+
+
+userRouter.post('/', async (req, res, next) => {
+  try {
+    const data = myPrisma.validate("User", req.body);
+    const newUser = await prisma.user.create({ data });
+    res.send(newUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+userRouter.patch('/:userId', async (req, res, next) => {
+  try {
+    const data = myPrisma.validate("User", req.body);
+    const editUser = await prisma.user.update({ 
+      where:{
+        id: parseInt(req.params.userId)
+      },
+      data });
+    res.send(editUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+userRouter.delete('/:userId', async (req, res, next) => {
+  try {
+    const deletedUser = await prisma.user.delete({ 
+      where:{
+        id: parseInt(req.params.userId)
+      },
+    }) 
+    res.send(deletedUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+  
 module.exports = userRouter
