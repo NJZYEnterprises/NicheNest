d91385c54fd7eeba382111081bebf9a81119808a
Alexander Linse <zandervon24@gmail.com>
2024-05-21 15:52:17 -0500

    Added more utility functions for objects

--- src/utils/myObj.cjs
+++ src/utils/myObj.cjs
@@ -19,6 +19,58 @@ const myObj = {
     const result = {};
     keys.forEach(k => result[k] = obj[k]);
     return result;
+  },
+  /**
+   * Checks key against existing object keys, 
+   * and returns a new key with some number added to denote a duplicate
+   * used in case duplicate keys from different sources need to be retained with distinct values
+   * @param {Object} obj to check key against
+   * @param {String} key to check object for
+   * @param {Number | default} maxDuplicates number of times to retry naming the key
+   * @returns 
+   */
+  numberDuplicateKey: (obj, key, maxDuplicates = 1000) => {
+    for (let i = 2; i <= maxDuplicates; i++) {
+      if (!obj.hasOwnProperty(key)) return key;
+      
+      const prevIStr = (i-1).toString();
+      if (key.endsWith(prevIStr)) key = key.slice(0, -prevIStr.length);
+      key = key + i.toString();
+    }
+  },
+  /**
+   * Flattens all sub-objects from values by combining key names 
+   * with the name of the sub-object + its property name, 
+   * such that no value in the flattened object will be a nested object by the end
+   * CAUTION: directly modifies the object
+   * @param {Object} obj to flatten 
+   * @param {Function | undefined} joinKeysFn callback function to customize the method 
+   * by which object key name and sub-object key name are joined parameters: (key, subKey)
+   * @returns the given object, which has been modified to be flat
+   */
+  flatten: (obj, joinKeysFn) => {
+    if (typeof(joinKeysFn) !== "function")
+      joinKeysFn = (key, subKey) => [key, subKey].join("_");
+    
+    let flattenQ = []
+    const resetQ = () => {
+      flattenQ = Object.keys(obj).filter(key => typeof(obj[key]) === "object");
+      console.log("flattenQ:", flattenQ);
+    }
+    resetQ();
+    while (flattenQ.length > 0) {
+      for (const flattenKey of flattenQ) {
+        for (const subKey in obj[flattenKey]) {
+          const newKey = myObj.numberDuplicateKey(obj, joinKeysFn(flattenKey, subKey));
+          obj[newKey] = obj[flattenKey][subKey];
+        }
+        delete obj[flattenKey];
+      }
+      
+      resetQ();
+    }
+    
+    return obj;
   }
 }
 
