1fb7e8d8038262fcb19d6f3b10bb712f09719cfa
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-05-31 09:14:23 -0500

    f ix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ import accountIcon from "../assets/account.svg";
 import logo from "../assets/logo.png";
 
 
-const Navbar = ({ searchQuery, setSearchQuery }) => {
+const Navbar = () => {
   const { userId, handleLogout} = useContext(AuthContext)
   
   return <div className="flex flex-col sticky top-0 p-3 sky-scene" style={{zIndex: 2000/*, backgroundImage: "linear-gradient(var(--primaryColor) 60%, var(--surfaceColor))"*/ }}>
