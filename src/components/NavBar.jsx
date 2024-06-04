0ba0e84eea1d803d20a9412603200241b1dc744b
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-06-04 09:53:30 -0500

    added logos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../auth/AuthProvider"
 import { UserContext } from "./UserProvider"
 import loginIcon from "../assets/login.svg";
 import accountIcon from "../assets/account.svg";
-import logo from "../assets/logo.png";
+import NicheNestLogo from "../assets/NicheNestLogo.png";
 import navbarTitle from "../assets/navbarTitle.png"
 
 
@@ -15,15 +15,13 @@ const Navbar = () => {
 
   const profileImage = user?.images.find(image => image.isProfile);
   const profileImageUrl = profileImage ? profileImage.image_url : null;
-
-console.log(profileImageUrl);
   
   return (
   <div className="flex flex-col flex-grow sticky top-0 sky-scene" style={{zIndex: 2000/*, backgroundImage: "linear-gradient(var(--primaryColor) 60%, var(--surfaceColor))"*/ }}>
     <div className="flex flex-col justify-around mx-10 m-2">
       <div className='flex justify-around items-center'>
           <Link to={'/'}>
-              <img src={logo} alt="Logo" className="h-28 w-38 rounded-lg shadow-2xl transition-transform duration-300 ease-in-out transform hover:scale-105" />
+              <img src={NicheNestLogo} alt="Logo" className="h-28 w-38 rounded-lg shadow-2xl transition-transform duration-300 ease-in-out transform hover:scale-105" />
           </Link>
       <div className="flex flex-col justify-center items-center mx-26 mb-2">
           <img className="h-20 w-full transition-transform duration-300 ease-in-out transform hover:scale-110" src={navbarTitle} />
